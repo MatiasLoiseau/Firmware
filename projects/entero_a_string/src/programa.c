c637f36a063d6007d3561aa9771329042f54205f
MatiasLoiseau <matiasloiseau@gmail.com>
2018-01-19 19:49:03 -0300

    Added entero_a_string convierte numeros enteros a strings

---
+++ projects/entero_a_string/src/programa.c
@@ -0,0 +1,95 @@
+/*============================================================================
+ * Licencia:
+ * Autor:
+ * Fecha:
+ *===========================================================================*/
+
+/*==================[inlcusiones]============================================*/
+
+#include "programa.h"   // <= su propio archivo de cabecera
+#include "sapi.h"       // <= Biblioteca sAPI
+
+/*==================[definiciones y macros]==================================*/
+
+/*==================[definiciones de datos internos]=========================*/
+
+/*==================[definiciones de datos externos]=========================*/
+
+/*==================[declaraciones de funciones internas]====================*/
+
+/*==================[declaraciones de funciones externas]====================*/
+
+/**
+ * C++ version 0.4 char* style "itoa":
+ * Written by Lukás Chmela
+ * Released under GPLv3.
+
+ */
+char* itoa(int value, char* result, int base) {
+   // check that the base if valid
+   if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+   char* ptr = result, *ptr1 = result, tmp_char;
+   int tmp_value;
+
+   do {
+      tmp_value = value;
+      value /= base;
+      *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+   } while ( value );
+
+   // Apply negative sign
+   if (tmp_value < 0) *ptr++ = '-';
+   *ptr-- = '\0';
+   while(ptr1 < ptr) {
+      tmp_char = *ptr;
+      *ptr--= *ptr1;
+      *ptr1++ = tmp_char;
+   }
+   return result;
+}
+
+/*==================[funcion principal]======================================*/
+
+// FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
+int main( void ){
+
+   // ---------- CONFIGURACIONES ------------------------------
+   // Inicializar y configurar la plataforma
+   boardConfig();
+
+   uartConfig( UART_USB, 115200 );
+
+   uint8_t i = 0;
+
+   uint8_t numeroEnString[10];
+
+   // ---------- REPETIR POR SIEMPRE --------------------------
+   while( TRUE )
+   {
+      // Si se presiona TEC1, enciende el LEDR
+      if( !(gpioRead(TEC1) ) ){
+         i++;
+      }
+
+      // Si se presiona TEC2, enciende el LEDR
+      if( !(gpioRead(TEC2) ) ){
+         itoa( i, numeroEnString, 10);
+         uartWriteString( UART_USB, numeroEnString );
+         uartWriteString( UART_USB, "\r\n" );
+         delay(500);
+      }
+
+   }
+
+   // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
+   // directamenteno sobre un microcontroladore y no es llamado/ por ningun
+   // Sistema Operativo, como en el caso de un programa para PC.
+   return 0;
+}
+
+/*==================[definiciones de funciones internas]=====================*/
+
+/*==================[definiciones de funciones externas]=====================*/
+
+/*==================[fin del archivo]========================================*/
